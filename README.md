0fb3601c754c144ce207b2fd3ad51055f1d60735
ZhuoyangM <zhmeng@ucsd.edu>
2022-10-26 15:10:13 -0700

    CI and Some explore

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Lab 5 
 ## Name: Zhuoyang Meng
 ## Partner: None 
+## [Link to Continuous Integration Repo](https://github.com/ZhuoyangM/introduction-to-github)
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,30 @@
 // explore.js
-
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  populateVoices();
+  
+}
+
+function populateVoices(){
+  let speechSynthesis=window.speechSynthesis;
+  
+  if (typeof speechSynthesis === 'undefined') {
+    return;
+  }
+  let voices = speechSynthesis.getVoices();
+  let select=document.getElementById('voice-select');
+  
+  for (let i=0;i<voices.length;i++){
+      let option=document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+      if (voices[i].default) {
+        option.textContent += ' — DEFAULT';
+      }
+  
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      select.appendChild(option);
+  }
 }
